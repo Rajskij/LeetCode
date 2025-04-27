65c5df11892476993d20dab95bd285f5551079a9
Igor <68941665+Rajskij@users.noreply.github.com>
2025-04-26 20:40:39 -0400

    Time: 1 ms (70.83%), Space: 42.2 MB (78.37%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -3,21 +3,17 @@ class Solution {
         if (!(str1 + str2).equals(str2 + str1)) {
             return "";
         }
-        int l1 = str1.length();
-        int l2 = str2.length();
-        int end = gsd(l1, l2);
+        int end = gsd(str1.length(), str2.length());
 
         return str1.substring(0, end);
     }
 
     private int gsd(int l1, int l2) {
-        if (l1 == 0) {
-            return l2;
-        }
-        if (l2 == 0) {
-            return l1;
-        }
-
-        return gsd(l2, l1 % l2);
+        while (l2 != 0) {
+            int tmp = l2;
+            l2 = l1 % l2;
+            l1 = tmp;
+        }    
+        return l1;
     }
 }
