9f6313f385e3b8938fee91da88231806a4659962
Igor <68941665+Rajskij@users.noreply.github.com>
2025-05-01 20:37:33 -0400

    Time: 6 ms (6.19%), Space: 44.9 MB (9.5%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -0,0 +1,32 @@
+// keys  1, 3
+// visited rooms 0
+// if key from visited room - remove key
+// keys is empty and length visited rom < roms.length return false else true
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Deque<Integer> keys = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        rooms.get(0).forEach(i -> keys.push(i));
+        visited.add(0);
+        
+        while (!keys.isEmpty()) {
+            int key = keys.pop();
+            keys.remove(key);
+            List<Integer> curKeys = new ArrayList<>();
+
+            if (key < rooms.size()) {
+                curKeys = rooms.get(key);
+                visited.add(key);
+            }
+
+            for (Integer k : curKeys) {
+                if (!visited.contains(k)) {
+                    keys.push(k);
+                }
+            }
+        }
+
+        return visited.size() == rooms.size();
+    }
+}
