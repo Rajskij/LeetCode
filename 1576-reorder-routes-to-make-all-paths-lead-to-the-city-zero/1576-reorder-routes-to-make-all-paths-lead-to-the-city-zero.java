4ca144742171ca5cc77795e8ed9b92a5efaca030
Igor <68941665+Rajskij@users.noreply.github.com>
2025-05-04 18:57:48 -0400

    Time: 36 ms (83.52%), Space: 73.2 MB (61.15%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,49 @@
+// we got 6 nodes
+// each node have relation one to one like list
+// so there is could be node1 -> node2 -> node3 or n1 <- n2 -> n3
+// i need to swap N nodes, so every node leads to n0.
+// 
+// so i can travel srough nodes starting from the first seans its n0(capital)
+// and then serch for next node assosiated with n0 and check if there is correct direction
+// - if not swap direction, and then gow to the next node marked 1 as visited
+// - then i can go to MAP that stores node as a key and asosiated nodes as a value and go though each value
+// 
+
+// map should be field as
+// 0 -> 1
+// 1 -> 3
+// 2 -> 3
+// 4 -> 0
+// 4 -> 5
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        List<List<Integer>> al = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            al.add(new ArrayList<>());
+        }
+        for (int[] conection : connections) {
+            int parent = conection[0];
+            int child = conection[1];
+            al.get(parent).add(child);
+            al.get(child).add(-parent);
+        }
+        return dfs(al, new boolean[n], 0);
+    }
+
+    private int dfs(List<List<Integer>> al, boolean[] visited, int node) {
+        int count = 0;
+        visited[node] = true;
+
+        for (Integer connection : al.get(node)) {
+            int con = Math.abs(connection);
+            if (!visited[con]) {
+                if (connection > 0) {
+                    count++;
+                }
+                count += dfs(al, visited, con);
+            }
+        }
+
+        return count;
+    }
+}
