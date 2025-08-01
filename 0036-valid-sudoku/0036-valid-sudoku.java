2b6a482d1a89603e1200c3d2b6f857e6d1fd3d9c
Igor <68941665+Rajskij@users.noreply.github.com>
2025-08-01 14:41:21 -0400

    Time: 4 ms (35.97%), Space: 44.4 MB (78.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -6,36 +6,29 @@ class Solution {
 
         for (int i = 0; i < board.length; i++) {
             rows.put(i, new HashSet<>());
-            
             for (int j = 0; j < board[0].length; j++) {
                 char c = board[i][j];
+                if (c != '.') {
+                    Set curRow = rows.get(i);
+                    Set curColumn = columns.computeIfAbsent(j, s -> new HashSet<>());
+                    int boxIdx = (i / 3) * 3 + j / 3;
+                    Set curBox = boxs.computeIfAbsent(boxIdx, s -> new HashSet<>());
+                    
+                    if (curRow.contains(c)) {
+                        return false;
+                    }
+                    if (curColumn.contains(c)) {
+                        return false;
+                    }
+                    if (curBox.contains(c)) {
+                        return false;
+                    }
 
-                if (c == '.') {
-                    continue;
+                    curRow.add(c);
+                    curColumn.add(c);
+                    curBox.add(c);
                 }
-
-                // check and collect rows
-                Set curRow = rows.get(i);
-                if (curRow.contains(c)) {
-                    return false;
-                }
-                curRow.add(c);
-
-                // check and collect columns
-                Set curColumn = columns.computeIfAbsent(j, s -> new HashSet<>());
-                if (curColumn.contains(c)) {
-                    return false;
-                }
-                curColumn.add(c);
-
-                // check and collect box
-                int boxIdx = (i / 3) * 3 + j / 3;
-                Set curBox = boxs.computeIfAbsent(boxIdx, s -> new HashSet<>());
-                if (curBox.contains(c)) {
-                    return false;
-                }
-                curBox.add(c);
-            }        
+            }
         }
 
         return true;
